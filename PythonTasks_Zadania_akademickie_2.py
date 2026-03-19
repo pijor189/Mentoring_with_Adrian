284ede77b547911472a2966ea7acd056c1c43f86
Krzysztof Pijor <krzysztof.pijor@molex.com>
2026-03-19 18:45:30 +0100

    Code cleanup

--- PythonTasks_Zadania_akademickie_2.py
+++ PythonTasks_Zadania_akademickie_2.py
@@ -7,50 +7,50 @@ zadanie = 1
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def obroc_string(napis: str) -> str:
-    return str([napis[x] for x in range(len(napis) - 1, -1, -1)])
-print(f"Napis: {"string"} odwrócony: {obroc_string("string")}")
+def reverse_string(text: str) -> str:
+    return str([text[x] for x in range(len(text) - 1, -1, -1)])
+print(f"Text: {"string"} reversed text: {reverse_string("string")}")
 
 # 2.	Napisz funkcję, która przyjmuje dwa stringi. Funkcja ma zwracać True, jeśli słowa są Anagramem.
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def anagram(text1: str, text2: str) -> bool:
+def is_anagram(text1: str, text2: str) -> bool:
     if sorted(text1.lower()) == sorted(text2.lower()):
         return True
     return False
 
 text1 = "karta"
 text2 = "krata"
-print(f"Czy dwa stringi: {text1} oraz {text2} są anagramami?\n{anagram(text1, text2)}")
+print(f"Are two string {text1} and {text2} anagrams?\n{is_anagram(text1, text2)}")
 text1 = "karta"
 text2 = "kraty"
-print(f"Czy dwa stringi: {text1} oraz {text2} są anagramami?\n{anagram(text1, text2)}")
+print(f"Are two string {text1} and {text2} anagrams?\n{is_anagram(text1, text2)}")
 
 # 3.	Naszym zadaniem jest zamiana ciągu `a4b3r6` na `aaaabbbrrrrrr`.
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-ciag = 'a4b3r6'
+sequence = 'a4b3r6'
 tmp = ''
-for i in range(0, len(ciag), 2):
-    tmp += ciag[i] * int(ciag[i + 1])
+for i in range(0, len(sequence), 2):
+    tmp += sequence[i] * int(sequence[i + 1])
 print(tmp)
 
 # 4.	Napisz dekorator, który wypisze „przed” przed wywołaniem funkcji i „po” po wywołaniu funkcji (musisz umieć dekoratory na swoją rozmowę)
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def dekorator(func):
-    def deko():
-        print("przed")
+def decorator(func):
+    def d():
+        print("Before")
         func()
-        print("po")
-    return deko
+        print("After")
+    return d
 
-@dekorator
+@decorator
 def show():
-    print("To jest główna część")
+    print("Hello")
 
 show()
 
@@ -58,16 +58,16 @@ show()
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def zlicz_litery(napis: str) -> int:
-    licznik = 0
-    for litera in napis:
-        if litera.isalpha():
-            licznik += 1
+def count_letters(text: str) -> int:
+    counter = 0
+    for letter in text:
+        if letter.isalpha():
+            counter += 1
 
-    return licznik
+    return counter
 
-napis = 'abcdefgh567--!  '
-print(f"W stringu {napis} jest {zlicz_litery(napis)} liter")
+text = 'abcdefgh567--!  '
+print(f"There are {count_letters(text)} letters in the {text}")
 
 """
 6.	Napisz funkcję, która (użyj context managera):
@@ -81,21 +81,21 @@ zadanie += 1
 
 import os
 
-def dopisz_do_pliku(plik, a):
-    if not os.path.isfile(plik):
-        return print(f"Błąd przy odczycie, sprawdz czy podałeś dobrze plik")
-    with open(plik, "r") as f:
+def append_to_file(file, content):
+    if not os.path.isfile(file):
+        return print(f"Error while reading, check if you provided the correct file")
+    with open(file, "r") as f:
         tmp = f.read()
-        print(f"Zawartość pliku przed zapisem {tmp}")
-        number = int(tmp) + a
+        print(f"File contents before saving {tmp}")
+        number = int(tmp) + content
 
-    with open(plik, "w") as f:
+    with open(file, "w") as f:
         f.write(str(number))
 
-    with open(plik, "r") as f:
-        print(f"Zawartość pliku po nadpisaniu {f.read()}")
+    with open(file, "r") as f:
+        print(f"File contents after saving {f.read()}")
 
-dopisz_do_pliku("plik_zad_6", 100)
+append_to_file("plik_zad_6", 100)
 
 """7.	Stwórz własny generator, który będzie wypisywał liczby ciągu fibonaciego dla zadanego N oznaczającego, ile ma ich wygenerować.
 wejście: 5
@@ -104,7 +104,7 @@ wyjście: 1, 1, 2, 3, 5
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def generuj_ciag_Fib(n: int) -> list:
+def generate_Fib_seq(n: int) -> list:
     tab = []
     for i in range(n):
         if i < 2:
@@ -114,7 +114,7 @@ def generuj_ciag_Fib(n: int) -> list:
     return tab
 
 n = 10
-print(f"Ciąg Fibonaciego dla podanego N = {n} -> {generuj_ciag_Fib(n)}")
+print(f"Fibonacci sequence for the given N ={n} -> {generate_Fib_seq(n)}")
 
 """
 8.	Napisz funkcję, która dostając listę, znajdzie i zwróci najczęściej występujący element. 
@@ -134,22 +134,22 @@ zadanie += 1
         if tmp1[val] is max(tmp1.values()):
             return val"""
 
-def znajdz_elem(lista: list):
+def find_elem(collection: list):
     # lista z unikalnymi wartościami
-    tmp = list(set(lista))
+    tmp = list(set(collection))
     # pierwszy element do porównań, obecnie traktowany jako najczęściej występujący
-    tmp1 = lista.count(tmp[0])
+    tmp1 = collection.count(tmp[0])
     # indeks elementu z unikalnej listy, która występuje najczęściej w liście podanej jako parametr funkcji
     tmp2 = 0
     for i in range(1, len(tmp)):
-        if tmp1 < lista.count(tmp[i]):
-            tmp1 = lista.count(tmp[i])
+        if tmp1 < collection.count(tmp[i]):
+            tmp1 = collection.count(tmp[i])
             tmp2 = i
 
     return tmp[tmp2]
 
-lista = [1, 2, 2, 3, 3, 3, 'a', 'a', 'a', 'a']
-print(f"Najczęściej występujący element z listy {lista} to: {znajdz_elem(lista)}")
+collection = [1, 2, 2, 3, 3, 3, 'a', 'a', 'a', 'a']
+print(f"The most frequent element in the list {collection} is: {find_elem(collection)}")
 
 """
 9.	Napisz funkcję która będzie sprawdzać, czy kolejność nawiasów się zgadza:
@@ -160,25 +160,25 @@ print(f"Najczęściej występujący element z listy {lista} to: {znajdz_elem(lis
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def czy_nawiasy_sa_ok(napis: str) -> bool:
-    pary = {'}' : '{', ']': '[', ')': '('}
+def are_brackets_ok(string: str) -> bool:
+    pairs = {'}' : '{', ']': '[', ')': '('}
     tab = []
-    for znak in napis.strip():
-        if znak in pary.values():
-            tab.append(znak)
-        elif znak in pary:
-            if tab[-1] == pary[znak]:
+    for char in string.strip():
+        if char in pairs.values():
+            tab.append(char)
+        elif char in pairs:
+            if tab[-1] == pairs[char]:
                 tab.pop()
             else:
                 return False
     return True
 
-nawiasy = "{}()[]"
-print(f"Czy kolejność nawiasów w tym przypadku {nawiasy} jest prawidłowa?: {czy_nawiasy_sa_ok(nawiasy)}")
-nawiasy = "({[]})"
-print(f"Czy kolejność nawiasów w tym przypadku {nawiasy} jest prawidłowa?: {czy_nawiasy_sa_ok(nawiasy)}")
-nawiasy = "(({((}})"
-print(f"Czy kolejność nawiasów w tym przypadku {nawiasy} jest prawidłowa?: {czy_nawiasy_sa_ok(nawiasy)}")
+string = "{}(a)b[]"
+print(f"Is the order of brackets correct in this case {string}?: {are_brackets_ok(string)}")
+string = "({[]})"
+print(f"Is the order of brackets correct in this case {string}?: {are_brackets_ok(string)}")
+string = "(({((}})"
+print(f"Is the order of brackets correct in this case {string}?: {are_brackets_ok(string)}")
 
 """
 10.	Napisz funkcję która doda do siebie dwie liczby zapisane jako stringi bez użycia int().
@@ -188,34 +188,34 @@ print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
 # przyznaje, nie miałem tutaj pomysłu jak to zrobić, myślałem o zastosowaniu ASCII, ale nie pomyślałem, by to zrobić jak z dodawaniem pod kreskę i wspomogłem się trochę
-def dodaj_stringi(napis1: str, napis2: str) -> str:
+def add_strings(string1: str, string2: str) -> str:
     # obliczamy rozmiary stringów oraz rozmiar pętli
-    len1 = len(napis1) - 1
-    len2 = len(napis2) - 1
-    size_of_loop = len(napis1) if len(napis1) > len(napis2) else len(napis2)
-    reszta_dodawania = 0
+    len1 = len(string1) - 1
+    len2 = len(string2) - 1
+    size_of_loop = len(string1) if len(string1) > len(string2) else len(string2)
+    rest_of_sum = 0
     tab = []
     for i in range(size_of_loop, 0, -1):
         # zczytujemy pojedynczo cyfry od końca i dodajemy do siebie jak w dodawaniu pod kreską
         # żeby je poprawnie zczytać, zamieniamy je na kod ASCII i odejmujemy wartość 0 w ASCII, by odzwierciedlić liczbę
-        tmp1 = ord(napis1[len1]) - ord('0') if len1 >= 0 else 0
-        tmp2 = ord(napis2[len2]) - ord('0') if len2 >= 0 else 0
-        suma_dodawania = tmp1 + tmp2 + reszta_dodawania
+        tmp1 = ord(string1[len1]) - ord('0') if len1 >= 0 else 0
+        tmp2 = ord(string2[len2]) - ord('0') if len2 >= 0 else 0
+        sum_of_addition = tmp1 + tmp2 + rest_of_sum
         # jeśli suma większa niż 9, przenosimy resztę do kolejnej iteracji
-        if suma_dodawania < 10:
-            reszta_dodawania = 0
+        if sum_of_addition < 10:
+            rest_of_sum = 0
         else:
-            reszta_dodawania = 1
-            suma_dodawania = suma_dodawania - 10
-        tab.append(str(suma_dodawania))
+            rest_of_sum = 1
+            sum_of_addition = sum_of_addition - 10
+        tab.append(str(sum_of_addition))
         len1 -= 1
         len2 -= 1
 
     return "".join(tab[::-1])
 
-napis1 = "140"
-napis2 = "80"
-print(f"{napis1} + {napis2} = {dodaj_stringi(napis1, napis2)}")
+string1 = "140"
+string2 = "80"
+print(f"{string1} + {string2} = {add_strings(string1, string2)}")
 
 """
 11.	Napisz funkcje sortującą krotki po drugim elemencie.
@@ -224,8 +224,8 @@ print(f"{napis1} + {napis2} = {dodaj_stringi(napis1, napis2)}")
 print(f"\nZadanie {zadanie}\n")
 zadanie += 1
 
-def sortuj_krotki_po_2_elemencie(a):
+def sort_tuples_on_sec_elem(a):
     return sorted(a, key=lambda x: x[1])
 
 a = [(1,3),(2,1),(3,2)]
-print(f"Krotki przed sortowaniem: {a}\nKrotki po sortowaniu po 2 elemencie: {sortuj_krotki_po_2_elemencie(a)}")
+print(f"Tuples before sorting: {a}\nTuples after sorting on the second element: {sort_tuples_on_sec_elem(a)}")
