8fdaba88ac994f8db79d17dba1ad4ec7da86dee2
Krzysztof Pijor <krzysztof.pijor@molex.com>
2026-05-28 14:17:35 +0200

    Tasks - API

---
+++ PythonTasks_API_1.py
@@ -0,0 +1,230 @@
+"""
+    Zadania z pliku Python_API_Tasks_1
+"""
+import json
+
+exercise = 1
+print(f"Exercise {exercise}")
+exercise += 1
+# 1.	Zapoznaj się z NBP Web API (przeczytaj o dostępnych opcjach) (https://api.nbp.pl/)
+# 2.	Za pomocą biblioteki request pobierz aktualny kurs euro. Pobierz go w formacie XML oraz JSON.
+# Z obu rodzajów danych wyciągnij dokładną wartość i przypisz ją do zmiennej typu float
+import requests
+import xmltodict
+
+print("\nXML")
+euro_xml = requests.get('https://api.nbp.pl/api/exchangerates/rates/a/eur/?format=xml')
+print(euro_xml.text)
+euro_xml = xmltodict.parse(euro_xml.text)
+euro_value_from_xml = euro_xml['ExchangeRatesSeries']['Rates']['Rate']['Mid']
+euro_value_from_xml = float(euro_value_from_xml)
+print(euro_value_from_xml)
+print(type(euro_value_from_xml))
+
+print("\nJSON")
+euro_json = requests.get('https://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json')
+print(type(euro_json))
+euro_json = euro_json.json()
+print(euro_json)
+euro_value_from_json = euro_json['rates'][0]['mid']
+print(euro_value_from_json)
+print(type(euro_value_from_json))
+
+# 3.	Napisz funkcję, która będzie pobierać żądany kurs. def get_exchange_rates(currency: str) -> float:
+print(f"Exercise {exercise}")
+exercise += 1
+
+def get_exchange_rates(currency: str) -> float:
+    data_a = requests.get(f"https://api.nbp.pl/api/exchangerates/tables/a/?format=json")
+    data_b = requests.get(f"https://api.nbp.pl/api/exchangerates/tables/b/?format=json")
+    data_a = data_a.json()
+    data_b = data_b.json()
+    data = data_a[0]['rates'] + data_b[0]['rates']
+
+    for val in data:
+        if currency == val['currency']:
+            return val['mid']
+    print("There is no currency like the one you provided")
+    return 0.0
+
+get_exchange_rates('bat')
+
+# 4.	Rozszerz funkcję o obsługę danych historycznych. def get_exchange_rates(currency: str,
+# date_from: str, date_to: str = None) -> float:
+print(f"Exercise {exercise}")
+exercise += 1
+
+from _datetime import date, timedelta
+
+def find_currency(currency: str) -> tuple[str, str] | None:
+    if not currency:
+        print("The provided currency is not valid.")
+        return None
+
+    data_a = requests.get(f"https://api.nbp.pl/api/exchangerates/tables/a/?format=json")
+    data_a = data_a.json()
+
+    for val in data_a[0]['rates']:
+        if currency == val['currency']:
+            return val['code'], 'a'
+    else:
+        data_b = requests.get(f"https://api.nbp.pl/api/exchangerates/tables/b/?format=json")
+        data_b = data_b.json()
+
+        for val in data_b[0]['rates']:
+            if currency == val['currency']:
+                return val['code'], 'b'
+        else:
+            print("404 Not Found\nThe provided currency is not available.")
+            return None
+
+
+def get_exchange_rates(currency: str, date_from: str, date_to: str = None) -> dict | None:
+    try:
+        code, table = find_currency(currency)
+    except TypeError:
+        print("404 Not Found\nThe provided currency is not available or is not valid")
+        return None
+
+    try:
+        if not date_to:
+            dt = date.today()
+        else:
+            dt = date.fromisoformat(date_to)
+
+        df = date.fromisoformat(date_from)
+
+        if dt > date.today():
+            raise ValueError
+
+        if df > dt:
+            raise ValueError
+
+    except ValueError:
+        print("400 Bad Request")
+        return None
+
+    exchange_rates = {}
+
+    if dt.year == df.year or (df.year + 1 == dt.year and df.month >= dt.month):
+        data = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/{table}/{code}/{df}/{dt}/')
+        data = data.json()
+
+        for val in data['rates']:
+            exchange_rates[val['effectiveDate']] = val['mid']
+    else:
+        actual_year = df.year
+
+        while True:
+            if actual_year == df.year:
+                end_year_date = f"{actual_year + 1}-01-01"
+                data = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/{table}/{code}/{df}/\
+{end_year_date}/')
+                data = data.json()
+
+                for val in data['rates']:
+                    exchange_rates[val['effectiveDate']] = val['mid']
+            elif actual_year == dt.year:
+                last_year_date = f"{dt.year}-01-01"
+                data = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/{table}/{code}/\
+{last_year_date}/{dt}/')
+                data = data.json()
+
+                for val in data['rates']:
+                    exchange_rates[val['effectiveDate']] = val['mid']
+
+                break
+            else:
+                data = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/{table}/{code}/{actual_year}\
+                -01-01/{actual_year + 1}-01-01/')
+                data = data.json()
+
+                for val in data['rates']:
+                    exchange_rates[val['effectiveDate']] = val['mid']
+
+                if f"{actual_year + 1}-01-01" == date_to:
+                    break
+
+            actual_year += 1
+    try:
+        if len(exchange_rates) == 0:
+            raise ValueError
+        return exchange_rates
+
+    except ValueError:
+        print("404 Not found")
+        return None
+
+
+print(get_exchange_rates('bat (Tajlandia)', '2027-01-01', '2026-01-01'))
+print(get_exchange_rates('balt (Tajlandia)', '2012-01-01', '2012-01-31'))
+
+# print(get_exchange_rates('bat (Tajlandia)', '2012-01-01', '2012-01-31'))
+# print(get_exchange_rates('bat (Tajlandia)', '2012-01-01', '2015-06-01'))
+# print(get_exchange_rates('bat (Tajlandia)', '2026-05-24'))
+
+# 5.	Zastanów się w jaki sposób obsłużyć przypadek, w którym podany jest tylko jeden dzień, a nie przedział.
+# Np. get_exchange_rates(„USD”, „2026-02-25”)
+# 6.	Do powyższej funkcji dopisz obsługę błędów adekwatną do komunikatów błędów obsługiwanych przez BNP.
+# (Poszerz ją o brakujące argumenty)
+# 7.	Utwórz trzy wywołania funkcji get_exchange_rates(), które udowodnią, że obsługa błędów działa prawidłowo.
+# 8.	Oblicz średni kurs Euro z ostatnich 15 dni.
+
+def get_date_from_days(days: int) -> str:
+    date_from = date.today() - timedelta(days)
+
+    return date_from.isoformat()
+
+def get_avg_currency(currency: str, days: int) -> float:
+    data = get_exchange_rates(currency, get_date_from_days(days))
+
+    return round(sum(data.values()) / len(data.values()), 2)
+
+print(f"Average currency of {'euro'} from {15} days is {get_avg_currency('euro', 15)}\n")
+
+# 9.	Sprawdź w jaki sposób zapisujemy typ formatu json do pliku. Zapisz w ten sposób kurs Euro z ostatnich 15 dni.
+
+def log_currency_to_file(currency: str, days: int) -> None:
+    data = get_exchange_rates(currency, get_date_from_days(days))
+
+    with open("materialy/euro_last_15_days.json", "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+log_currency_to_file('euro', 15)
+
+# 10.	Napisz pseudo-aplikacje w terminalu (wykorzystaj input()), która będzie pytać użytkownika, ile ma złotych
+# i na jaką walutę chciałby je przewalutować. Aplikacja ta ma zwrócić przewalutowaną wartość.
+
+def currency_exchange(balance: int | float, currency: str) -> int | float | None:
+    try:
+        code, _ = find_currency(currency)
+    except:
+        print("Not valid currency")
+        return None
+
+    data = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/c/{code}/today/')
+    data = data.json()
+
+    return round(balance / data['rates'][0]['ask'], 2)
+
+# zloty = int(input("How many zloty you have?: \n"))
+# currency = input("What currency you want?: \n")
+# print(f"{zloty} złoty -> {currency_exchange(zloty, currency)} {currency}")
+
+# 11.	Za pomocą matplot narysuj wykres zmian kursu GBP w przeciągu ostatnich 60dni.
+import matplotlib.pyplot as plt
+
+data = get_exchange_rates('funt szterling', get_date_from_days(60))
+x = [d for d, _ in enumerate(data.keys(), start=1)]
+y = [c for c in data.values()]
+
+plt.plot(x, y, marker='o', color='b', linestyle='-', linewidth=2)
+plt.title("Wykres zmian kursu GDB")
+plt.xlabel("Dni")
+plt.ylabel("Kursy")
+plt.grid(True)
+plt.show()
+
+# 12.	API NBP ma limit 93 dni dla zapytań. Jeśli zapytanie je przekroczy, spraw, aby Twoje rozwiązanie
+# automatycznie dzieliło je na mniejsze.
+
---
+++ PythonTasks_API_1_2.py
@@ -0,0 +1,135 @@
+"""
+Disclaimer Ta sekcja ma na celu pokazać Ci inne API, a pech też chciał, że jest ono bardzo nieprzyjemne dla użytkownika
+i posiada relatywnie mało wiedzy w Internecie. Biorąc to pod uwagę, weź te zadania na luzie, bardziej jako wyzwanie do
+zabawy z którym trzeba wiele eksperymentować i szukać samemu rozwiązań – w razie czego pytaj :).
+Nasz Endpoint to - https://dane.gov.pl/<api_ver>/<rest of the path> dane.gov.pl api
+Na potrzeby zadania znalazłem bazę danych na Strona główna - Otwarte Dane w której znajdują się imiona męskie i żeńskie
+ nadawane dzieciom w 2025. Patrząc na link, możemy znaleźć interesujący nasz resource id - 1187350, którego użyjemy w
+  naszym API. Tabela ta zawiera 25864 wpisy. Imiona pierwsze nadane dzieciom w Polsce w 2025 r. wg USC - Otwarte Dane
+  – zapoznaj się z tym jaki rodzaj innych danych zawiera ta tabela - poza imieniem.
+
+13.	Do sprawdzenia formatu danych na pobranych danych, możesz użyć response.headers["Content-Type"]. Typ danych który
+ otrzymujemy powinien być typu application/……json. Jeśli nie jest, napisz do mnie :) (sam się tu mocno zaskoczyłem)
+14.	Moduł request, posiada również interesujący parametr nazywający się param. Pozwala on nam na dodanie słów kluczy
+w wyszukiwarce, które znajdują się za przecinkiem  . Pozwoli Ci to na filtrowanie danych które są zwracane.
+Korzystając z tej wiedzy pobierz 1000 wpisów z bazy imion.
+"""
+import time
+
+import requests
+
+# data = []
+# count = int(1000 / 50)
+# url = 'https://api.dane.gov.pl/1.4/resources/1187350/data'
+#
+# for key in range(1, count + 1):
+#     response = requests.get(url ,params={"page": 1, "per_page": 50})
+#     content_type = response.headers.get("Content-Type", "")
+#     if "application/vnd.api+json" in content_type:
+#         payload = response.json()
+#         data += payload.get("data", [])
+#
+# for item in data:
+#     print(item['attributes'])
+
+"""
+15.	Korzystając z samego modułu request pobierz najczęściej oraz najrzadziej występujące męskie i damskie imię.
+(odp. Amadeusz=2, Jan=632, Anastazja=2, Zofia=475)
+(Dopuszczam, że mogą się tu pojawić inne imiona dla minimalnej ilości.)
+"""
+data = []
+page = 1
+url = 'https://api.dane.gov.pl/1.4/resources/1187350/data'
+
+start_time = time.time()
+
+while True:
+    params = {
+        "page": page,
+        "per_page": 50
+    }
+    response = requests.get(url, params=params)
+    payload = response.json()
+    records = payload.get("data")
+
+    if not records:
+        break
+
+    data += records
+    page += 1
+
+print(len(data))
+
+man_names = {}
+woman_names = {}
+
+child_name = 'DAMIAN'
+specific_name_dict = {child_name: {}}
+
+for item in data:
+    name = item['attributes']['col5']['val']
+    gender = item['attributes']['col6']['val']
+    count = item['attributes']['col7']['val']
+    state = item['attributes']['col2']['val']
+    if gender == 'KOBIETA':
+        woman_names[name] = woman_names.get(name, 0) + int(count)
+    else:
+        man_names[name] = man_names.get(name, 0) + int(count)
+
+    if name == child_name:
+        specific_name_dict[child_name][state] = specific_name_dict[child_name].get(state, 0) + int(count)
+
+print(specific_name_dict)
+
+print(woman_names)
+
+min_woman_count = len(woman_names)
+min_woman_name = ''
+max_woman_count = 0
+max_woman_name = ''
+
+for key, val in woman_names.items():
+    if val < min_woman_count:
+        min_woman_count = val
+        min_woman_name = key
+    if val > max_woman_count:
+        max_woman_count = val
+        max_woman_name = key
+
+print(min_woman_name, int(min_woman_count))
+print(max_woman_name, int(max_woman_count))
+
+min_man_count = len(man_names)
+min_man_name = ''
+max_man_count = 0
+max_man_name = ''
+
+for key, val in man_names.items():
+    if val < min_man_count:
+        min_man_count = val
+        min_man_name = key
+    if val > max_man_count:
+        max_man_count = val
+        max_man_name = key
+
+print(min_man_name, int(min_man_count))
+print(max_man_name, int(max_man_count))
+
+count = 0
+max_state = {'state': None, 'count': None}
+
+for key, val in specific_name_dict[child_name].items():
+    if val > count:
+        count = val
+        max_state['state'] = key
+        max_state['count'] = val
+
+print(child_name, max_state)
+
+end_time = time.time()
+print(f"Execution time: {end_time - start_time}")
+
+"""
+16.	Przefiltruj dane dla imienia Damian. W jakim Województwie nadano to imię najczęściej oraz w jakiej ilości?
+17.	Stwórz swoje własne zapytanie :).
+"""
